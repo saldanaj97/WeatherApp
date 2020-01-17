e600fb3b959ee244c8d4919779e1c662e7640b2a
Juan Saldana <saldanaj97@gmail.com>
2020-01-16 20:45:24 -0600

    Added two more weather blocks for tomorrow & 2+.

--- app.js
+++ app.js
@@ -4,33 +4,69 @@ const weatherkey = "3b4cd95a1216d611c0362d7c9a949388";
 const googlekey = "AIzaSyBP1QHmFQma8LErOOPfa9rZy0f-gvFhoEA";
 
 // Select Elements
-const notificationElement = document.querySelector(".notification");
-const iconElement = document.querySelector(".weather-icon");
-const descElement = document.querySelector(".temperature-description p");
-const tempElement = document.querySelector(".temperature-value p");
-const tempfeelslikeElement = document.querySelector(".temperature-feels-like p");
-const locationElement = document.querySelector(".location p");
+const currNotificationElement = document.querySelector(".curr-notification");
+const currIconElement = document.querySelector(".curr-weather-icon");
+const currDescElement = document.querySelector(".curr-temperature-description p");
+const currTempElement = document.querySelector(".curr-temperature-value p");
 
-// Default Weather Data
-const weather = {
+const nextNotificationElement = document.querySelector(".next-notification");
+const nextIconElement = document.querySelector(".next-weather-icon");
+const nextDescElement = document.querySelector(".next-temperature-description p");
+const nextTempElement = document.querySelector(".next-temperature-value p");
+
+const twoDayNotificationElement = document.querySelector(".twoDay-notification");
+const twoDayIconElement = document.querySelector(".twoDay-weather-icon");
+const twoDayDescElement = document.querySelector(".twoDay-temperature-description p");
+const twoDayTempElement = document.querySelector(".twoDay-temperature-value p");
+
+const titleElement = document.querySelector(".title-bar");
+
+// Default Weather Data (forecast[0] represents the current day, forecast[1] represents tomorrow, etc...)
+var forecast = [];
+forecast.push({
     temperature : {
         value: 50,
         feelsLike: 50, 
         unit: "fahrenheit"
     },
-    description: "Few Clouds",
+    description: "Default",
     iconID: "01d",
-    city: "Dallas",
-    state: "TX"
-};
+    city: "Default",
+    state: "Default"
+}, 
+// This section represents tomorrow 
+{
+    temperature : {
+        value: 50,
+        unit: "fahrenheit"
+    },
+    description: "Default",
+    iconID: "01d"
+}, 
+// This section represents 2 days later
+{
+    temperature : {
+        value: 50,
+        unit: "fahrenheit"
+    },
+    description: "Default",
+    iconID: "01d"
+});
+
 
 // Location Data
 if("geolocation" in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
 }
 else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't support geolocation.</p";
+    currNotificationElement.style.display = "block";
+    currNotificationElement.innerHTML = "<p>Browser doesn't support geolocation.</p";
+
+    nextNotificationElement.style.display = "block";
+    nextNotificationElement.innerHTML = "<p>Browser doesn't support geolocation.</p";
+
+    twoDayNotificationElement.style.display = "block";
+    twoDayNotificationElement.innerHTML = "<p>Browser doesn't support geolocation.</p";
 }
 
 // Function to convert to title case found at https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
@@ -51,14 +87,17 @@ function getLocation(latitude, longitude){
         return data;
     })
     .then(function(data){
-        weather.city = data.results[0].address_components[2].long_name;
-        weather.state = data.results[0].address_components[4].short_name;
+        forecast[0].city = data.results[5].address_components[0].long_name;
+        forecast[0].state = data.results[5].address_components[2].short_name;
+
+        // Update the current location in the title bar 
+        titleElement.innerHTML = `3 Day Forecast For ${forecast[0].city}, ${forecast[0].state}`;
     });
 }
 
 
-// Function to get the weather from the open weather API
-function getWeather(latitude, longitude){
+// Function to get the current weather from the open weather API
+function getCurrWeather(latitude, longitude){
     let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherkey}`;
 
     // Parse the data from the API
@@ -68,70 +107,88 @@ function getWeather(latitude, longitude){
         return data;
     })
     .then(function(data){
-        weather.description = toTitleCase(data.weather[0].description);
-        weather.temperature.value = Math.floor(data.main.temp - KELVIN) * 9/5 + 32;
-        weather.temperature.feelsLike = Math.floor(data.main.feels_like - KELVIN) * 9/5 + 32;
-        weather.iconID = data.weather[0].icon;
+        forecast[0].description = toTitleCase(data.weather[0].description);
+        forecast[0].temperature.value = Math.floor(data.main.temp - KELVIN) * 9/5 + 32;
+        forecast[0].iconID = data.weather[0].icon;
     })
     .then(function(){
         displayWeather();
-    });
+    })
+}
+
+// Function to get the next days weather from the open weather API
+function getNextWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weatherkey}`;
+
+    // Parse the data (only use the 12:00:00 time for the next day)
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        forecast[1].description = toTitleCase(data.list[6].weather[0].description);
+        forecast[1].temperature.value = Math.floor(data.list[6].main.temp - KELVIN) * 9/5 + 32;
+        forecast[1].iconID = data.list[6].weather[0].icon;
+    })
+    .then(function(){
+        displayWeather();
+    })
+}
+
+// Function to get weather for +2 days
+function getTwoDayWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${weatherkey}`;
+
+    // Parse the data (only use the 12:00:00 time for the next day)
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        forecast[2].description = toTitleCase(data.list[14].weather[0].description);
+        forecast[2].temperature.value = Math.floor(data.list[14].main.temp - KELVIN) * 9/5 + 32;
+        forecast[2].iconID = data.list[14].weather[0].icon;
+    })
+    .then(function(){
+        displayWeather();
+    })
 }
 
 // Function to display the weather
 function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconID}.png"/>`;
-    descElement.innerHTML = `${weather.description}`;
-    tempElement.innerHTML = `${weather.temperature.value} ˚ <span> F</span>`;
-    tempfeelslikeElement.innerHTML = `Feels Like: ${weather.temperature.feelsLike} ˚ <span> F</span>`;
-    locationElement.innerHTML = `${weather.city}, ${weather.state}`;
+    currIconElement.innerHTML = `<img src="icons/${forecast[0].iconID}.png"/>`;
+    currDescElement.innerHTML = `${forecast[0].description}`;
+    currTempElement.innerHTML = `${forecast[0].temperature.value} ˚ <span> F</span>`;
+
+    nextIconElement.innerHTML = `<img src="icons/${forecast[1].iconID}.png"/>`;
+    nextDescElement.innerHTML = `${forecast[1].description}`;
+    nextTempElement.innerHTML = `${forecast[1].temperature.value} ˚ <span> F</span>`;
+
+    twoDayIconElement.innerHTML = `<img src="icons/${forecast[2].iconID}.png"/>`;
+    twoDayDescElement.innerHTML = `${forecast[2].description}`;
+    twoDayTempElement.innerHTML = `${forecast[2].temperature.value} ˚ <span> F</span>`;
 }
-
-// If user clicks the temperature, change the units
-tempElement.addEventListener("click", function(){
-    // Unit not defined
-    if(weather.temperature.unit === undefined){
-        return;
-    }
-
-    // Temperature is in fahrenheit (Fahrenheit -> Celsius)
-    if(weather.temperature.unit == "fahrenheit"){
-        // Actual temperature
-        var celsius = fahrenheitToCelsius(weather.temperature.value);
-        celsius = Math.floor(celsius);
-        tempElement.innerHTML = `${celsius}˚<span> C</span>`;
-
-        // Feels like temperature
-        var celsius = fahrenheitToCelsius(weather.temperature.feelsLike);
-        celsius - Math.floor(celsius);
-        tempfeelslikeElement.innerHTML = `Feels Like: ${celsius}˚<span> C</span>`;
-
-        weather.temperature.unit = "celsius";
-    }
-    else{
-        // Temperature is in celsius (Celsius -> Farenheit)
-        tempElement.innerHTML = `${weather.temperature.value}˚<span> F</span>`;
-        tempfeelslikeElement.innerHTML = `Feels Like: ${weather.temperature.feelsLike}˚<span> F</span>`;
-        weather.temperature.unit = "fahrenheit"
-    }
-});
 
 // Function to set the position
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     getLocation(latitude, longitude);
-    getWeather(latitude, longitude);
-
+    getCurrWeather(latitude, longitude);
+    getNextWeather(latitude, longitude);
+    getTwoDayWeather(latitude, longitude);
 }
 
 // Function to display error message to user
 function showError(error){
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
-}
+    currNotificationElement.style.display = "block";
+    currNotificationElement.innerHTML = `<p> ${error.message} </p>`;
 
-// Temp Conversions
-function fahrenheitToCelsius(temperature){
-    return (temperature - 32) * 5/9;
+    nextNotificationElement.style.display = "block";
+    nextNotificationElement.innerHTML = `<p> ${error.message} </p>`;
+
+    twoDayNotificationElement.style.display = "block";
+    twoDayNotificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
